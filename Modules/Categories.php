ebd57f0b5af039820a80eab3b6163b187d3ba381
mehmet70 <86437411+mufasa80@users.noreply.github.com>
2021-11-29 19:44:24 +0100

    updates
    meer updates

--- Modules/Categories.php
+++ Modules/Categories.php
@@ -2,10 +2,19 @@
 // TODO Zorg dat de methodes goed ingevuld worden met de juiste queries.
 function getCategories()
 {
-    
+    global $pdo;
+    $categories =$pdo->query('select * FROM categories')->fetchAll( PDO::FETCH_CLASS,'Category');
+    return $categories;
 }
 
 function getCategoryName(int $id)
 {
-    
+    global $pdo;
+    $sth = $pdo->prepare('Select * FROM categories WHERE ID=?');
+    $sth-> bindParam(1,$id,PDO::PARAM_INT);
+    $sth->execute();
+    $category=$sth->fetch(PDO::FETCH_ASSOC);
+    return $category['Title'];
+
+
 }
--- Modules/Products.php
+++ Modules/Products.php
@@ -2,10 +2,22 @@
 // TODO Zorg dat de methodes goed ingevuld worden met de juiste queries.
 function getProducts(int $categoryId)
 {
-    
+    global $pdo;
+    $sth = $pdo->prepare('SELECT * FROM products WHERE category_id=?');
+    $sth->bindParam(1,$categoryId);
+    $sth->execute();
+    return $sth->fetchAll(PDO::FETCH_CLASS,'Product');
+
+
+
 }
 
 function getProduct(int $productId)
+
 {
-    
+    global $pdo;
+    $sth = $pdo->prepare('SELECT * FROM products WHERE ID=?');
+    $sth->bindParam(1,$productId);
+    $sth->execute();
+    return $sth->fetchAll(PDO::FETCH_CLASS,'product')[0];
 }
